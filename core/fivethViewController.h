6c3814a0fd1ceaadd53c62847857ea33a836b5f8
Ning <747778937@qq.com>
2013-12-18 14:03:12 +0800

    core.1.2

---
+++ core/fivethViewController.h
@@ -0,0 +1,13 @@
+//
+//  fivethViewController.h
+//  core
+//
+//  Created by Ning on 13-12-18.
+//  Copyright (c) 2013年 Ning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fivethViewController : WCCCoreDataManager
+
+@end
---
+++ core/fivethViewController.m
@@ -0,0 +1,13 @@
+//
+//  fivethViewController.m
+//  core
+//
+//  Created by Ning on 13-12-18.
+//  Copyright (c) 2013年 Ning. All rights reserved.
+//
+
+#import "fivethViewController.h"
+
+@implementation fivethViewController
+
+@end
